f1d41ec22e42186fc96243047b4253e6257ccc7f
satyam_py <satyampd5340@gmail.com>
2020-07-08 02:36:16 +0530

    add the layouts and partials

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const port = 8000;
 
 const app = express();
-
+const expressLayouts = require("express-ejs-layouts");
+app.use(expressLayouts);
+//set view engine and set the view folder
+app.set("view engine","ejs");
+app.set("views","views");
 //use express router
 
 app.use("/",require("./routes/index"));
